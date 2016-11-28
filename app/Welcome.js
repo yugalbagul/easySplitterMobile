59dc709c23b25c39b17ffe9b133b8cf36b1aa9ce
Yugal <yugalbagul2@gmail.com>
2016-11-28 19:58:02 +0530

    #2 Es-lint integrates

---
+++ app/Welcome.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from  'react-native'
+
+export class Welcome extends React.Component {
+ render () {
+  return (
+    <Text> Welcome Bro </Text>
+  )
+ }
+}
