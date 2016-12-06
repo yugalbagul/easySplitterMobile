05e533857e76e936e7fb74ab33155a353ee28a64
Yugal <yugalbagul2@gmail.com>
2016-12-06 19:02:55 +0530

    #5 Initial Commit for the issue

--- app/Welcome.js
+++ app/Welcome.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Text } from  'react-native'
+import { MY_NAME } from './constants';
 
 export class Welcome extends React.Component {
  render () {
   return (
-    <Text> Welcome Bro </Text>
+    <Text> Welcome Bro { MY_NAME } </Text>
   )
  }
 }
